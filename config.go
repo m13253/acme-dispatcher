b4f59d15c66bdcf27ee3c3ac20e78d1c24c464d0
Star Brilliant <coder@poorlab.com>
2017-11-25 01:47:23 +0800

    Initial commit

---
+++ config.go
@@ -0,0 +1,58 @@
+/*
+    ACME-dispatcher -- Dispatch ACME challenge for a multihomed server
+    Copyright (C) 2017 Star Brilliant <m13253@hotmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package main
+
+import (
+	"log"
+	"os"
+	"github.com/BurntSushi/toml"
+)
+
+type config struct {
+	Listen				string		`toml:"listen"`
+	Path				string		`toml:"path"`
+	Forward				[]string	`toml:"forward"`
+	CircularPrevention	string		`toml:"circular_prevention"`
+}
+
+func loadConfig(path string) (*config, error) {
+	conf := &config {}
+	metaData, err := toml.DecodeFile(path, conf)
+	if err != nil {
+		return nil, err
+	}
+	undecoded := metaData.Undecoded()
+	for _, key := range metaData.Undecoded() {
+		log.Printf("unknown option %q\n", key.String())
+	}
+	if len(undecoded) != 0 {
+		os.Exit(1)
+	}
+
+	if conf.Listen == "" {
+		conf.Listen = "[::1]:44046"
+	}
+	if conf.Path == "" {
+		conf.Path = "/.well-known/acme-challenge"
+	}
+	if conf.CircularPrevention == "" {
+		conf.CircularPrevention = "X-ACME-Dispatcher"
+	}
+	return conf, nil
+}
---
+++ server.go
@@ -0,0 +1,132 @@
+/*
+    ACME-dispatcher -- Dispatch ACME challenge for a multihomed server
+    Copyright (C) 2017 Star Brilliant <m13253@hotmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+	"github.com/gorilla/handlers"
+)
+
+type server struct {
+	conf		*config
+	servemux	*http.ServeMux
+}
+
+func newServer(conf *config) *server {
+	s := &server {
+		conf: conf,
+		servemux: http.NewServeMux(),
+	}
+	s.servemux.HandleFunc(s.conf.Path, s.handlerFunc)
+	return s
+}
+
+func (s *server) Start() error {
+	return http.ListenAndServe(s.conf.Listen, handlers.CombinedLoggingHandler(os.Stdout, s.servemux))
+}
+
+func (s *server) handlerFunc(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" && r.Method != "HEAD" {
+		w.Header().Set("Allow", "GET, HEAD")
+		http.Error(w, "Method Not Allowed", 405)
+		return
+	}
+	if r.Header.Get(s.conf.CircularPrevention) == "1" {
+		http.Error(w, "Not Found", 404)
+		return
+	}
+	ctx, cancel := context.WithCancel(context.TODO())
+	path := r.URL.Path
+	if !strings.HasPrefix(path, "/") {
+		path = "/" + path
+	}
+	clientAddr, err := net.ResolveTCPAddr("tcp", r.RemoteAddr)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal Error", 500)
+		return
+	}
+	respChan := make(chan *http.Response)
+	var wg sync.WaitGroup
+	for _, upstream := range s.conf.Forward {
+		wg.Add(1)
+		go func(ctx context.Context, upstream, path string) {
+			defer func() {
+				recover()
+				wg.Done()
+			}()
+			req, err := http.NewRequest(r.Method, upstream + path, nil)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			req = req.WithContext(ctx)
+			for k, v := range r.Header {
+				if k != "Accept-Encoding" && k != "Content-Encoding" && k != "Connection" && k != "Proxy-Connection" {
+					req.Header[k] = v
+				}
+			}
+			req.Header.Set("X-Real-IP", clientAddr.IP.String())
+			xff := req.Header.Get("X-Forwarded-For")
+			if xff != "" {
+				xff = xff + "," + clientAddr.IP.String()
+			} else {
+				xff = clientAddr.IP.String()
+			}
+			req.Header.Set("X-Forwarded-For", xff)
+			req.Header.Set(s.conf.CircularPrevention, "1")
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			if resp.StatusCode < 500 && resp.StatusCode != 404 {
+				respChan <- resp
+			}
+		}(ctx, upstream, path)
+	}
+	go func() {
+		defer func() {
+			recover()
+		}()
+		wg.Wait()
+		close(respChan)
+	}()
+	resp, ok := <-respChan
+	if !ok {
+		http.Error(w, "Bad Gateway", 502)
+		return
+	}
+	close(respChan)
+	cancel()
+	respHeader := w.Header()
+	for k, v := range resp.Header {
+		if k != "Accept-Encoding" && k != "Content-Encoding" && k != "Connection" && k != "Proxy-Connection" {
+			respHeader[k] = v
+		}
+	}
+	io.Copy(w, resp.Body)
+}
